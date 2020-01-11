2d299212372cfa801e9a762e1427275e2a8cfb9c
Kayxue <s0990006@gmail.com>
2020-01-11 17:31:05 +0800

    2020-01-11 Manjaro

--- userinfo/views.py
+++ userinfo/views.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 # Create your views here.
 class Userinfo(LoginRequiredMixin, TemplateView):
     template_name="userinfo.html"
-
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = '使用者資訊'
